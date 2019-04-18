3051c43f2a77c16d5f18238e1432aa6882ed2278
mouri <tt.mouri@gmail.com>
2019-04-18 11:44:10 +0900

    apply material-ui to eventsIndex

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import './index.css';
 import reducer from './reducers';
@@ -14,24 +15,28 @@ import EventsShow from './components/events_show';
 import registerServiceWorker from './registerServiceWorker';
 
 const enhancer = process.env.NODE_ENV === 'development' ?
-  composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
+    composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
 const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/events/new" component={EventsNew} />
-        <Route path="/events/:id" component={EventsShow} />
-        <Route exact path="/" component={EventsIndex} />
-        <Route exact path="/events" component={EventsIndex} />
-      </Switch>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root'));
+    <MuiThemeProvider>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/events/new" component={EventsNew} />
+                    <Route path="/events/:id" component={EventsShow} />
+                    <Route exact path="/" component={EventsIndex} />
+                    <Route exact path="/events" component={EventsIndex} />
+                </Switch>
+            </BrowserRouter>
+        </Provider>
+    </MuiThemeProvider>,
+    document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
 registerServiceWorker();
+
